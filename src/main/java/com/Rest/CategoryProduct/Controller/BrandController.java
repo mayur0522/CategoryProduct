7e843391bfe672e30bf3e3236ec16853659ff802
Yogesh Jathar <109969909+Yogeshjathar@users.noreply.github.com>
2025-05-07 11:53:35 +0530

    Updated code

--- src/main/java/com/Rest/CategoryProduct/Controller/BrandController.java
+++ src/main/java/com/Rest/CategoryProduct/Controller/BrandController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/brand")
-public class brandController {
+public class BrandController {
 
     @Autowired
     private BrandService brandService;
