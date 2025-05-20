148948079c86b4bd13d9a124e4991fc550388dba
Yogesh Jathar <109969909+Yogeshjathar@users.noreply.github.com>
2025-05-20 12:31:20 +0530

    Update CategoryProductApplicationTests.java

--- src/test/java/com/Rest/CategoryProduct/CategoryProductApplicationTests.java
+++ src/test/java/com/Rest/CategoryProduct/CategoryProductApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class CategoryProductApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
